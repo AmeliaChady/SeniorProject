301aec0b7a013ef97e8be0a9c8a3be6e389db78c
AmeliaChady <6AnGp9C*mFdj>
2021-03-02 11:43:11 -0500

    added space for points creation to keep tests clean

---
+++ SegmentedLeastSquares_CreatePoints.py
@@ -0,0 +1,13 @@
+from SegmentedLeastSquares import *
+
+def create_linear_points():
+    pass
+
+def create_split_points():
+    pass
+
+def create_double_split_points():
+    pass
+
+def create_non_whole_points():
+    pass
---
+++ SegmentedLeastSquares_Test.py
@@ -0,0 +1,6 @@
+
+
+linear_points = []
+split_points = []
+double_split_points = []
+non_whole_points = []
