5b51940c40e78778b626ddadbdbf77a686c5bb15
AmeliaChady <6AnGp9C*mFdj>
2021-03-08 21:56:25 -0500

    implemented least squares and least squares error

--- SegmentedLeastSquares.py
+++ SegmentedLeastSquares.py
@@ -1,9 +1,44 @@
 from collections import namedtuple
 
 Coordinates = namedtuple('Coordinates', ['x', 'y'])
+Line = namedtuple('Line', ['a', 'b'])
+
+
+def least_squares(coordinate_list):
+    sum_xy = 0
+    sum_x = 0
+    sum_y = 0
+    sum_xsq = 0
+    n = len(coordinate_list)
+
+    for i in range(n):
+        coord = coordinate_list[i]
+        sum_x += coord.x
+        sum_y += coord.y
+        sum_xy += coord.x * coord.y
+        sum_xsq += coord.x ** 2
+
+    a = (n * sum_xy - sum_x * sum_y)/(n * sum_xsq - sum_x ** 2)
+    b = (sum_y - a * sum_x)/n
+    return Line(a, b)
+
+
+def least_squares_error(coordinate_list, line):
+    error = 0
+    for i in range(len(coordinate_list)):
+        coord = coordinate_list[i]
+        error += (coord.y - line.a * coord.x - line.b)**2
+    return error
+
 
 '''
 Takes in a set of points and returns a set of points marking the ends of each line segment in the solutions
 '''
 def segmented_least_squares(coordinate_list):
-    return [Coordinates(1, 2), Coordinates(3, 4)]
+    line = least_squares(coordinate_list)
+    start_coords = Coordinates(coordinate_list[0].x, line.a * coordinate_list[0].x + line.b)
+    end_coords = Coordinates(coordinate_list[-1].x, line.a * coordinate_list[-1].x + line.b)
+    return [start_coords, end_coords]
+
+
+
--- SegmentedLeastSquares_Test.py
+++ SegmentedLeastSquares_Test.py
@@ -4,19 +4,19 @@ from SegmentedLeastSquares import *
 print("\nLinear Points")
 coordinates = segmented_least_squares(create_linear_points())
 for coord in coordinates:
-    print(str(coord.x) + "\t" + str(coord.y))
+    print(str(round(coord.x, 2)) + "\t" + str(round(coord.y, 2)))
 
 print("\nSplit Points")
 coordinates = segmented_least_squares(create_split_points())
 for coord in coordinates:
-    print(str(coord.x) + "\t" + str(coord.y))
+    print(str(round(coord.x, 2)) + "\t" + str(round(coord.y, 2)))
 
 print("\nDouble Split Points")
 coordinates = segmented_least_squares(create_double_split_points())
 for coord in coordinates:
-    print(str(coord.x) + "\t" + str(coord.y))
+    print(str(round(coord.x, 2)) + "\t" + str(round(coord.y, 2)))
 
 print("\nNon Whole Numbers")
 coordinates = segmented_least_squares(create_non_whole_points())
 for coord in coordinates:
-    print(str(coord.x) + "\t" + str(coord.y))
+    print(str(round(coord.x, 2)) + "\t" + str(round(coord.y, 2)))
